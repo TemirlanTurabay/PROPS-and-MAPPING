ae0c2ac50f856a82c4da808d8ac06f0b00f58816
TemirlanTurabay <123382026+TemirlanTurabay@users.noreply.github.com>
2024-03-22 01:47:34 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -4,17 +4,52 @@ import Header from './Header';
 import Intro from './Intro';
 import Footer from './Footer';
 
+const posts = [
+  {
+    id: 1,
+    author: "Authors name",
+    topic: "Topics name",
+    date: "7 july",
+    title: "7 Practical CSS Tips",
+    text: "You not only learn more Python by implementing what you already know but, in the end, you can see how all your hard work pays off.",
+    timing: "12 min read",
+    type: "Selected for you"
+  },
+  {
+    id: 2,
+    author: "Authors name",
+    topic: "Topics name",
+    date: "7 july",
+    title: "7 Practical CSS Tips",
+    text: "You not only learn more Python by implementing what you already know but, in the end, you can see how all your hard work pays off.",
+    timing: "12 min read",
+    type: "Selected for you"
+  },
+  {
+    id: 3,
+    author: "Authors name",
+    topic: "Topics name",
+    date: "7 july",
+    title: "7 Practical CSS Tips",
+    text: "You not only learn more Python by implementing what you already know but, in the end, you can see how all your hard work pays off.",
+    timing: "12 min read",
+    type: "Selected for you"
+  }
+];
+
 function App() {
   return (
     <div>
       <Header/>
       <Intro/>
       <div id="cardscolumn" style={{ display: 'flex', flexDirection: 'column', width: '1440px', height: '1152px', padding: '0px 160px 0px 160px', gap: '40px' }}>
-        <Post/>
+      {posts.map(function(post, index){
+        return <Post key = {index} author = {post.author} topic = {post.topic} date = {post.date} title = {post.title} text = {post.text} timing = {post.timing} type = {post.type}/>
+      })}
       </div>
       <Footer/>
     </div>
   );
 }
 
-export default App;
+export default App
